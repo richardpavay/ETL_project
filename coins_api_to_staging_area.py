550a00c9e16108ef9ebf68dad8d7417bb0ab5da0
Richárd Pávay <shurgan15@gmail.com>
2023-05-10 20:23:28 +0200

    Testing the workflow

--- coins_api_to_staging_area.py
+++ coins_api_to_staging_area.py
@@ -46,4 +46,4 @@ conn = create_engine('mysql+mysqldb://root:Placeholder2023@localhost/coins')
 
 # Inserting DataFrame to MySQL database
 df.to_sql(con=conn,name='coin_prices',if_exists='append',index=False)
-# Testing the workflow
+# Testing if the workflows work
