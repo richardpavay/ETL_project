567514b0fca40f30b3856b8c7ccd9d0c2631a29c
Richárd Pávay <119036961+richardpavay@users.noreply.github.com>
2023-05-26 09:46:05 +0200

    Update stock_api_to_staging_area.py
    - Removed redundant SQL connector
    - Reorganized constants

--- stock_api_to_staging_area.py
+++ stock_api_to_staging_area.py
@@ -1,13 +1,15 @@
 # importing libraries
 import pandas as pd
-import mysql.connector
 import requests
 from sqlalchemy import create_engine
 
+# setting up sql connection
+CONN = create_engine('mysql+mysqldb://root:Placeholder2023@localhost/coins')
+
 # Setting up API connection, Retrieving JSON
 # key = '6ZWN2TPRC621WDV7'
-stock_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=6ZWN2TPRC621WDV7'
-r = requests.get(stock_url)
+STOCK_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=6ZWN2TPRC621WDV7'
+r = requests.get(STOCK_URL)
 stock_json = r.json()
 
 # Extracting data from JSON
@@ -18,16 +20,7 @@ stock_name = stock_json['Meta Data']['2. Symbol']
 # Transforming data to Python dictionary, the to pandas dataframe
 dict_stock = {'stock' : [stock_name], 'price' : [stock_price], 'date' : [today]}
 df_stock = pd.DataFrame(dict_stock)
-print(df_stock)
-
-# Setting up MySQL connection
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password='Placeholder2023'
-)
-
-conn = create_engine('mysql+mysqldb://root:Placeholder2023@localhost/coins')
+# print(df_stock)
 
 # inserting data to MYSQL database
-df_stock.to_sql(con=conn,name='stock_prices',if_exists='append',index=False)
+df_stock.to_sql(con=CONN,name='stock_prices',if_exists='append',index=False)
